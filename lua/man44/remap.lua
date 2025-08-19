2310f557aeb1d4f2a177b64cd04a41f8d5d88c08
xc19 <man44@tutamail.com>
2025-08-19 18:45:37 +0530

    remap.lua

---
+++ lua/man44/remap.lua
@@ -0,0 +1,18 @@
+local opts = { noremap = true, silent = true }
+
+
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- select all
+vim.keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- no need for tab selection remap as I have harpoon
+
+
+-- splitting screen
+vim.keymap.set("n", "ss", ":split<Return>", opts)
+vim.keymap.set("n", "sv", ":vsplit<Return>", opts)
+
+-- removing the highlights
+vim.keymap.set("n", "<leader>h", ":nohlsearch<CR>", opts)
