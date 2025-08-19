4bb4c3005a0a7ee2461046a48a52aea0ac1c1f96
xc19 <man44@tutamail.com>
2025-08-19 18:42:48 +0530

    lualine.lua

---
+++ after/plugin/lualine.lua
@@ -0,0 +1,18 @@
+-- gruvbox lualine config
+
+require("lualine").setup{
+	options = {
+		icons_enabled = false,
+		theme = "onedark",
+		component_separators = "|",
+		section_separators = "",
+	},
+}
+
+-- tokyonight lualine config
+
+-- require('lualine').setup {
+--   options = {
+--     theme = 'tokyonight'
+--   }
+-- }
